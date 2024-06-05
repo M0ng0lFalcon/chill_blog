a64426b81a634c4028ab9075d73c92ab2a563903
YUSHI如风 <bnd1013@qq.com>
2024-06-05 18:52:35 +0800

    Update:将SiteBlock导入进HomeView在主页中展示

--- chill_blog_src/src/views/HomeView.vue
+++ chill_blog_src/src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script>
+import SiteBlock from "./SiteBlock.vue";
+export default {
+  components:{
+    SiteBlock,
+  }
+}
+</script>
 
 <template>
   <main>
+    <SiteBlock />
     <h1>this is home page</h1>
   </main>
 </template>
