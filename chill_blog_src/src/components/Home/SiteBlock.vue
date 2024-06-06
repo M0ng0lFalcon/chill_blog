6854e8a5d0e5e8347d0a9d8cf055686d0c6a3c72
M0ng0lFalcon <M0ng0lF4lc0n@protonmail.ch>
2024-06-06 16:04:23 +0800

    Fix: introduce -> description

--- chill_blog_src/src/components/Home/SiteBlock.vue
+++ chill_blog_src/src/components/Home/SiteBlock.vue
@@ -3,19 +3,19 @@
     <ButtonBlock
       website="https://blog.csdn.net/YUSHI_wind?spm=1018.2226.3001.5343"
       site_name="csdn"
-      introduce="学习笔记"
+      description="学习笔记"
       img_src="../../public/csdnlogo.jpg"
     />
     <ButtonBlock
       website="https://www.zhihu.com/people/F4lc0n"
       site_name="知乎"
-      introduce="不懂就问"
+      description="不懂就问"
       img_src="../../public/zhihulogo.jpg"
     />
     <ButtonBlock
       website="https://space.bilibili.com/162901193?spm_id_from=333.1007.0.0"
       site_name="b站"
-      introduce="billbill大学"
+      description="billbill大学"
       img_src="../../public/billlogo.jpg"
     />
   </div>
