c38993cd0a6a55f67ae1a8a4c6b0829ae5b11087
M0ng0lFalcon <M0ng0lF4lc0n@protonmail.ch>
2024-06-06 16:03:38 +0800

    Update: 更新主页布局

--- chill_blog_src/src/views/HomeView.vue
+++ chill_blog_src/src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import SiteBlock from './SiteBlock.vue'
+import MenuVertical from '@/components/ui/MenuVertical.vue'
+import SiteBlock from '@/components/Home/SiteBlock.vue'
 </script>
 
 <template>
-  <main>
-    <SiteBlock />
-    <h1>this is home page</h1>
+  <main class="flex h-screen w-screen p-5">
+    <div class="w-18">
+      <MenuVertical></MenuVertical>
+    </div>
+
+    <div class="flex-1">
+      <SiteBlock />
+    </div>
   </main>
 </template>
